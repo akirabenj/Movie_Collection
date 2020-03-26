1c76edb8a89361e01d518c455fdfc041fa511245
Admin <admin@Mac-Admin.local>
2020-03-26 16:57:23 +0300

    Initial Commit

---
+++ Movie_Collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie_Collection
+//
+//  Created by Admin on 26.03.2020.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
